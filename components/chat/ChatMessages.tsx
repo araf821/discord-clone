d43dfe0a73bbef4385d6c9e5900981fc174bd780
araf821 <araf.ahmed200@gmail.com>
2023-09-03 19:38:53 -0400

    implemented real time updates with web sockets and socket.io

--- components/chat/ChatMessages.tsx
+++ components/chat/ChatMessages.tsx
@@ -7,6 +7,7 @@ import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
 import ChatItem from "./ChatItem";
 import { format } from "date-fns";
+import { useChatSocket } from "@/hooks/use-chat-socket";
 
 interface ChatMessagesProps {
   name: string;
@@ -40,6 +41,8 @@ const ChatMessages: FC<ChatMessagesProps> = ({
   socketQuery,
 }) => {
   const queryKey = `chat:${chatId}`;
+  const addKey = `chat:${chatId}:messages`;
+  const updateKey = `chat:${chatId}:messages:update`;
 
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
     useChatQuery({
@@ -48,6 +51,8 @@ const ChatMessages: FC<ChatMessagesProps> = ({
       paramValue,
       queryKey,
     });
+
+  useChatSocket({ queryKey, addKey, updateKey });
 
   if (status === "loading") {
     return (
