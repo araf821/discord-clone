91a25567b2bcf31a0cd95a8c07db90ec2cc3e82e
araf821 <araf.ahmed200@gmail.com>
2023-09-02 21:23:29 -0400

    initialized converation page between members

--- components/chat/ChatHeader.tsx
+++ components/chat/ChatHeader.tsx
@@ -1,6 +1,7 @@
 import { Hash, Menu } from "lucide-react";
 import { FC } from "react";
 import MobileToggle from "../MobileToggle";
+import UserAvatar from "../UserAvatar";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -20,6 +21,9 @@ const ChatHeader: FC<ChatHeaderProps> = ({
       <MobileToggle serverId={serverId} />
       {type === "channel" && (
         <Hash className="mr-2 h-5 w-5 text-zinc-500 dark:text-zinc-400" />
+      )}
+      {type === "conversation" && (
+        <UserAvatar src={imageUrl} className="mr-2 h-8 w-8 md:h-8 md:w-8" />
       )}
       <p className="text-md font-semibold text-black dark:text-white">{name}</p>
     </div>
